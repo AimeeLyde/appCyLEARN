5e4b26556c59afb30dd15ccab68a7f6399292326
AimeeLyde <seoulouaimeedesiree@hotmail.fr>
2024-05-21 09:41:27 +0200

    commit

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,9 +1,1 @@
-// import User from '../models/UserSchema.js';
-// export const updateUser = async(req,res)=>{
-//     const id = req.params.id
-//     try {
-//         const updateUser = await User.findByIdAndUpdate(id, {$set:req.body})
-//     } catch (err) {
-        
-//     }
-// }
+// 
